00c885ea4eb2faef5191cd110e4a5603bd04590f
tetalisriteja439 <54357545+tetalisriteja439@users.noreply.github.com>
2025-09-26 14:17:17 -0500

    Added an inline function to resize the image in about.jsx

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -14,7 +14,7 @@ const About = () => {
         transition={{duration: 0.5}}
           className="w-full lg:w-1/2 lg:p-8">
           <div className="flex items-center justify-center">
-            <img className="rounded-2xl " src={abtPic} alt="me" />
+            <img className="rounded-2xl" src={abtPic} alt="me" style={{ width: '500px', height: '550px' }} />
           </div>
         </motion.div>
         <motion.div 
